4c17ab9531881d3b194de3558bab173ef7664e57
andiabdillahdev <andiabdillahdev@gmail.com>
2022-04-24 20:58:45 +0700

    revisi kegiatan again

--- app/Http/Controllers/kegiatanController.php
+++ app/Http/Controllers/kegiatanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\kegiatan;
+use App\Models\pegawai;
 use Validator;
 use Auth;
 class kegiatanController extends Controller
