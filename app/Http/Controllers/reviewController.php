9bdeae6384a285537dfe200c260ca29aa17bcdcc
andiabdillahdev <andiabdillahdev@gmail.com>
2022-04-26 00:24:51 +0700

    cek

--- app/Http/Controllers/reviewController.php
+++ app/Http/Controllers/reviewController.php
@@ -22,9 +22,9 @@ class reviewController extends Controller
         // return $getData;
 
         $myArray = [];
-        $getDataStatus = [];
         $status = '';
         foreach ($getData as $key => $value) {
+            $getDataStatus = [];
             $res = DB::table('tb_pegawai')->select('tb_pegawai.nama', 'tb_pegawai.nip', 'tb_pegawai.jenis_jabatan', 'tb_pegawai.id AS id_pegawai','tb_review.kesesuaian AS kesesuaian','tb_skp.id AS id_skp')->join('tb_skp','tb_pegawai.id', '=', 'tb_skp.id_pegawai')->join('tb_review','tb_skp.id','=','tb_review.id_skp')->where('id_pegawai',$value->id_pegawai)->get();
             
             if (isset($res)) {
