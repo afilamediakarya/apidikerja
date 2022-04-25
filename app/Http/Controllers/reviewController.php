9adcbd2bcfa4bcacc1ee66b686d6972e1c7a2772
andiabdillahdev <andiabdillahdev@gmail.com>
2022-04-26 01:54:37 +0700

    semoga

--- app/Http/Controllers/reviewController.php
+++ app/Http/Controllers/reviewController.php
@@ -24,14 +24,18 @@ class reviewController extends Controller
         if (isset($jabatanPegawai)) {
             $getData = DB::table('tb_jabatan')->where('parent_id',$jabatanPegawai->id)->get(); 
 
-            return $getData;   
+            // return $getData;   
 
 
             $status = '';
             foreach ($getData as $key => $value) {
-                $getDataStatus = [];
+
+                if ($value->id_pegawai != null) {
+
+                      $getDataStatus = [];
                 $res = DB::table('tb_pegawai')->select('tb_pegawai.nama', 'tb_pegawai.nip', 'tb_pegawai.jenis_jabatan', 'tb_pegawai.id AS id_pegawai','tb_review.kesesuaian AS kesesuaian','tb_skp.id AS id_skp')->join('tb_skp','tb_pegawai.id', '=', 'tb_skp.id_pegawai')->join('tb_review','tb_skp.id','=','tb_review.id_skp')->where('id_pegawai',$value->id_pegawai)->get();
                 
+
                     // $myArray[$key] = $res;
 
                     if (isset($res)) {
@@ -56,6 +60,10 @@ class reviewController extends Controller
                                 'status' => $status
                             ];
                     }
+                    
+                }
+
+              
             }
 
         }
