07c3afbec9811462c485c11e22e65f234a221538
andiabdillahdev <andiabdillahdev@gmail.com>
2022-05-23 19:59:28 +0700

    ap

--- app/Http/Controllers/jabatanController.php
+++ app/Http/Controllers/jabatanController.php
@@ -64,7 +64,7 @@ class jabatanController extends Controller
         $data->parent_id = $request->parent_id;
         $data->nama_jabatan = $request->nama_jabatan;
         $data->status_jabatan = $request->status_jabatan;
-        $data->pembayaran_tpp = $request->pembayaran_tpp;
+        $data->pembayaran_tpp = str_replace(',','',$request->pembayaran_tpp);
         $data->save();
 
         if ($data) {
@@ -120,7 +120,7 @@ class jabatanController extends Controller
         $data->parent_id = $request->parent_id;
         $data->nama_jabatan = $request->nama_jabatan;
         $data->status_jabatan = $request->status_jabatan;
-        $data->pembayaran_tpp = $request->pembayaran_tpp;
+        $data->pembayaran_tpp = str_replace(',','',$request->pembayaran_tpp);
         $data->save();
 
         if ($data) {
