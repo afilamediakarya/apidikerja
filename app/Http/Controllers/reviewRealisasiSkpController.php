e64f56cc8cf9d1e12068f15c13297951366ac260
andiabdillahdev <andiabdillahdev@gmail.com>
2022-08-01 14:31:37 +0800

    dashboard

--- app/Http/Controllers/reviewRealisasiSkpController.php
+++ app/Http/Controllers/reviewRealisasiSkpController.php
@@ -13,57 +13,13 @@ class reviewRealisasiSkpController extends Controller
 {
 
     public function list(){
-        // $getData = atasan::where('id_penilai',Auth::user()->id_pegawai)->get();
         $jabatanPegawai = DB::table('tb_jabatan')->select('id')->where('id_pegawai',Auth::user()->id_pegawai)->first();
         $myArray = [];
         $groupId = [];
          $groupSkpPegawai = [];
         if (isset($jabatanPegawai)) {
-            $getData = DB::table('tb_jabatan')->where('parent_id',$jabatanPegawai->id)->get(); 
-        
-            $status = '';
-            foreach ($getData as $key => $value) {
-
-                if (!is_null($value->id)) {
-                   array_push($groupId,$value->id);       
-                }  
-             }
-
-             foreach ($groupId as $x => $vv) {
-                $res = DB::table('tb_pegawai')->select('tb_pegawai.id','tb_pegawai.nama', 'tb_pegawai.nip', 'tb_pegawai.jenis_jabatan', 'tb_skp.id AS id_skp', 'tb_pegawai.id AS id_pegawai','tb_review.kesesuaian AS kesesuaian')->join('tb_jabatan','tb_pegawai.id','=','tb_jabatan.id_pegawai')->join('tb_skp','tb_jabatan.id', '=', 'tb_skp.id_jabatan')->join('tb_review','tb_skp.id','=','tb_review.id_skp')->where('tb_jabatan.id',$vv)->get();
-
-                if (count($res) > 0) {
-                    array_push($groupSkpPegawai,$res);
-                } 
-
-              }      
+            $myArray = DB::table('tb_jabatan')->select('tb_jabatan.id','tb_jabatan.id_pegawai','tb_jabatan.nama_jabatan','tb_pegawai.nama','tb_pegawai.nip')->join('tb_pegawai','tb_jabatan.id_pegawai','=','tb_pegawai.id')->where('parent_id',$jabatanPegawai->id)->get();  
         }
-
-        // return $groupSkpPegawai;
-
-          foreach ($groupSkpPegawai as $bnb => $llo) {
-            $getDataStatus = [];
-              foreach ($llo as $vv => $bb) {
-                 $getReview = review_realisasi_skp::where('id_skp',$bb->id_skp)->get()->pluck('kesesuaian')->toArray();
-                }
-                  if (in_array("tidak", $getReview) == true && in_array("ya", $getReview) == true){
-                    $status = 'Belum Sesuai';
-                }
-                else if(in_array("ya", $getReview) == true && in_array("tidak", $getReview) == false){
-                    $status = 'Selesai';
-                }else{
-                    $status = 'Belum Review';
-                }
-
-                $myArray[$bnb] = [
-                    'nama'=>$llo[0]->nama,
-                    'nip'=>$llo[0]->nip,
-                    'jenis_jabatan'=>$llo[0]->nama_jabatan,
-                    'id_pegawai'=>$llo[0]->id_pegawai,
-                    'status'=>$status,
-                ];
-
-          }
 
         if ($myArray) {
             return response()->json([
