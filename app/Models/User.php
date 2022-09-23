0d2c3a9ad78d06a785ed999658248dd75568e88f
andiabdillahdev <andiabdillahdev@gmail.com>
2022-09-23 19:44:51 +0800

    users model revisi

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable,HasApiTokens;
-    protected $with = ['pegawai'];
+    // protected $with = ['pegawai'];
 
     /**
      * The attributes that are mass assignable.
